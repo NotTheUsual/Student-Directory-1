018fbaf83603c7c21b89aa42ce4188e16c981a04
Peter Saxton <peterhsaxton@gmail.com>
2014-01-08 09:11:54 +0000

    Program now askes to input students

--- directory.rb
+++ directory.rb
@@ -19,9 +19,26 @@ end
 
 def print_footer(names)
 	puts "Overall we have #{names.length} great students"
-end\
+end
 
+def input_students(cohort)
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
 
+	students = []
+
+	name = gets.chomp
+
+	while !name.empty? do
+		students << {:name => name, cohort: cohort}
+		puts "Now we have #{students.length} students"
+
+		name = gets.chomp
+	end
+	students
+end
+
+students = input_students(:November)
 
 print_header()
 print_names(students)
