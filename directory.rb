266490372d29784ddef3f39c43476da0bbc6bfd0
Peter Saxton <peterhsaxton@gmail.com>
2014-01-08 09:04:53 +0000

    Changed student entries to hashes

--- directory.rb
+++ directory.rb
@@ -1,9 +1,9 @@
 students = [
-	"Me",
-	"James",
-	"Andrey",
-	"Ming the merciless",
-	"Donald Duck"
+	{name: "Me", cohort: :November},
+	{name: "James", cohort: :November},
+	{name: "Andrey", cohort: :November},
+	{name: "Ming the merciless", cohort: :July},
+	{name: "Donald Duck", cohort: :July}
 ]
 
 def print_header
@@ -13,13 +13,15 @@ end
 
 def print_names(names)
 	names.each do |student|
-		puts student
+		puts "#{student[:name]} (#{student[:cohort]})"
 	end
 end
 
 def print_footer(names)
 	puts "Overall we have #{names.length} great students"
-end
+end\
+
+
 
 print_header()
 print_names(students)
